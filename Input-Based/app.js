5ba649bb3df01759252ec7ddd52ae61c21dc1e11
BorMooh <bor.mohoric2@gmail.com>
2021-05-25 22:51:03 +0200

    Added timezones, local time

--- Input-Based/app.js
+++ Input-Based/app.js
@@ -6,6 +6,7 @@ window.addEventListener('load', ()=>
     //Elementi iz HTML
     let dataDegrees = document.querySelector('.data-degrees');
     let dataDescription = document.querySelector('.description');
+    let dataTimezone = document.querySelector('.timezone');
 
     //API
     const api = `http://api.openweathermap.org/data/2.5/weather?q=${inputLocation}&appid=16f364f0a3b530faec39488f8a34aab3`;
@@ -38,11 +39,19 @@ window.addEventListener('load', ()=>
 
             //Geografski podatki o lokaciji
             let mesto = data.name
-            let timezone = data.timezone;
+            let timezone = getTimeZone(data.timezone);
+            let localTime = getTime(timezone);
             let lat = data.coord.lat;
             let lon = data.coord.lon;
 
 
+            console.log(timezone);
+            console.log(localTime);
+            //Time - test
+            dataTimezone.textContent = `UTC ${timezone}`;
+
+
+            //
 
             dataDegrees.textContent = tempC;
             dataDescription.textContent = weatherDesc;
@@ -64,4 +73,23 @@ function convertToFahr(kelvin)
 {
     Number.parseInt(kelvin, 10);
     return (((kelvin - 273.15) * 1.8) + 32).toFixed(1);
+}
+
+function getTimeZone(seconds)
+{
+    Number.parseInt(seconds, 10);
+    return (seconds / 3600); 
+}
+
+function getTime(timezone)
+{
+    console.log(timezone);
+    let today = new Date();
+    let hours = today.getUTCHours();
+    let minutes = today.getUTCMinutes();
+
+
+    hours = Number.parseInt(hours, 10) + Number.parseInt(timezone, 10);
+
+    return `${hours}:${minutes}`;
 }
